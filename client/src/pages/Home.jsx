7f741faf3c6bd999f3f639e04753d1b588d7d91b
Avishkar Mohite <avimohite2019@gmail.com>
2022-03-25 18:13:42 +0530

    no words

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,34 +19,29 @@
 // };
 
 // export default Home;
-import React from 'react'
-import Header1 from './Header1';
-import Featurs from './Featurs';
-import Hero from './Hero';
-import Footer from './Footer';
-import Faq from './Faq';
-import '../assets/template1.css';
+import React, { useState } from "react";
+import Header1 from "./Header1";
+import Featurs from "./Featurs";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Faq from "./Faq";
+import "../assets/template1.css";
 
 export const Home = () => {
+  const [data, setData] = useState();
 
-
-
-
-    
   return (
-   <>
-     <header className="header">
+    <>
+      <header className="header">
         <div className="overlay has-fade hide-for-desktop" />
         <Header1 />
-        </header>
-        <Hero />
-        <Featurs />
-        <Faq />
-        <Footer />
-        
-   
-   </>
-  )
-}
+      </header>
+      <Hero data={data} setData={setData} />
+      <Featurs />
+    
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
